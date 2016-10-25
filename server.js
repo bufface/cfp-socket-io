5fcfb2be340616c9167cdef518f8cd39e3ae6efc
Cristian Buffa <bufface@gmail.com>
2016-10-25 17:19:45 -0300

    Video 3: Enviar y maquetar index

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ var http = require('http');
 
 var app = express();
 
+app.get('/', function (req, res) {
+  res.sendFile('index.html', {'root': __dirname});
+})
+
 var server = http.createServer(app);
 
 server.listen(8080);
