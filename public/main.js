eda511c27a07842706ab63315ac6845822c7824d
Cristian Buffa <bufface@gmail.com>
2016-10-26 17:00:39 -0300

    Video 7: Quitar código a scope global

--- public/main.js
+++ public/main.js
@@ -2,44 +2,46 @@
   true => juega con las X
   false => juega con los O
 */
-var juego = false;
+(function () {
+  var juego = false;
 
-function $(el) {
-  return document.querySelector(el);
-}
-
-function jugar(elemento_seleccionado) {
-  if (juego){
-    elemento_seleccionado.innerHTML = 'X';
-  } else {
-    elemento_seleccionado.innerHTML = '0';
-  } 
-}
-
-function definir_eventos() {
-  var elements = document.querySelectorAll('.cat-element');
-
-  for(var i = 0; i<elements.length; i++) {
-    var element = elements[i];
-
-    element.addEventListener('click', function () {
-      jugar(this);
-    })
-  }
-}
-
-function build_cat() {
-  for (var i = 0; i<9; i++) {
-    var item = build_item(i);
-
-    $('#cat').innerHTML += item; 
+  function $(el) {
+    return document.querySelector(el);
   }
 
-  definir_eventos();
-}
+  function jugar(elemento_seleccionado) {
+    if (juego) {
+      elemento_seleccionado.innerHTML = 'X';
+    } else {
+      elemento_seleccionado.innerHTML = '0';
+    }
+  }
 
-function build_item(i) {
-  return '<div class="cat-element col-xs-4" id="elemento-'+ i +'"></div>'
-}
+  function definir_eventos() {
+    var elements = document.querySelectorAll('.cat-element');
 
-build_cat();
+    for (var i = 0; i < elements.length; i++) {
+      var element = elements[i];
+
+      element.addEventListener('click', function () {
+        jugar(this);
+      })
+    }
+  }
+
+  function build_cat() {
+    for (var i = 0; i < 9; i++) {
+      var item = build_item(i);
+
+      $('#cat').innerHTML += item;
+    }
+
+    definir_eventos();
+  }
+
+  function build_item(i) {
+    return '<div class="cat-element col-xs-4" id="elemento-' + i + '"></div>'
+  }
+
+  build_cat();
+})();
