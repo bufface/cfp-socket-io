240861ad96f50a4f4efe39d8bd268922b8b36527
Cristian Buffa <bufface@gmail.com>
2016-10-26 16:40:54 -0300

    Video 5: Contruir grid con JS

--- public/main.js
+++ public/main.js
@@ -1,1 +1,17 @@
-console.log('Hola Mundo');
+function $(el) {
+  return document.querySelector(el);
+}
+
+function build_cat() {
+  for (var i = 0; i<9; i++) {
+    var item = build_item(i);
+
+    $('#cat').innerHTML += item; 
+  }
+}
+
+function build_item(i) {
+  return '<div class="cat-element col-xs-4" id="elemento-'+ i +'"></div>'
+}
+
+build_cat();
